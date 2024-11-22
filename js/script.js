99440531cb4a291f8114446f3adf8ea8bf72b71e
Alexia Salas <alexsalas0420@gmail.com>
2024-11-22 12:45:34 -0600

    Tranformar a AWP

--- js/script.js
+++ js/script.js
@@ -63,3 +63,12 @@ function cleanText(text) {
 textArea.addEventListener('input', function() {
     textArea.value = cleanText(textArea.value);
 });
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
