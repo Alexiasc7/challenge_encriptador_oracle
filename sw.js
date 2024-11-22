0e7cd072d2d1f018f1067f93d2a5427775c105e3
Alexia Salas <alexsalas0420@gmail.com>
2024-11-22 13:32:53 -0600

    fotos cambios

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ const assetsToCache = [
     './index.html',
     './css/style.css',
     './js/script.js',
-    './assets/Logo.png'
+    './assets/Logo-160-192.png',
+    './assets/Logo-427-512.png'
 ];
 
 self.addEventListener('install', (event) => {
